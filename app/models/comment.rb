b0530bdb251b5e66e5ba634b89a2e09c071d4cca
Judd Nutting <juddnutting@gmail.com>
2015-05-21 05:48:43 +0000

    create associations between users, posts, comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
-class Post < ActiveRecord::Base
+class Comment < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  belongs_to :post
+  
   validates :body, presence: true
   validates :user_id, presence: true
+  validates :post_id, presence: true
   
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+ 
+ def setup
+  @post = Post.new(body: "this is some text", user_id: 1)
+ end
+ 
+ test "post should be valid" do
+   assert @post.valid?
+ end
+ 
+ test "post should have a user" do
+   assert @post.user_id
+ end
+ 
+ 
   # test "the truth" do
   #   assert true
   # end
